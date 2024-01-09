5a3fc284c49a08c3e366d56d8086df73d1e16744
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-01-09 21:04:40 +0400

    Initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    function index()
+    {
+        $post = Post::find(1);
+        dd($post);
+    }
+}
